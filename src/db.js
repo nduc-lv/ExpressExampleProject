e746b3f584fb07a292cf63d5d395b99d00067a76
Vu <longvunguyen2003@gmail.com>
2024-05-07 14:24:43 +0700

    s

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const {MongoClient} = require("mongodb");
 const client = new MongoClient(process.env.MONGODB_CONNECT_STRING, {
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
 });
 const connection = client.connect();
 module.exports = connection;
